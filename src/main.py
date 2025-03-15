b9f7b86e2268e79ab3cdec9c407782f7c86912b6
Vlt09 <valentin.barrau@free.fr>
2025-03-15 22:20:55 +0100

    feat: begin managing depth

--- src/main.py
+++ src/main.py
@@ -6,10 +6,19 @@ from detect_ball import appCalibration
 
 
 def main():
+
+    first_frame = True
+
+    # Define variable to estimate depth and rotation
+    depth_step = 1
+    first_area = float()
+    delta_oscillation_max = 10000
+    z = 0
+    fixed_keypoints = None
+
     # Find ball color bounds
     # lowBounds, highBounds = appCalibration()
     lowBounds, highBounds = (22, 57, 88), (83, 255, 255)
-
     orb = cv2.ORB_create(15)
 
     # Open the default camera
@@ -20,8 +29,6 @@ def main():
     frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
     cv2.namedWindow('windows')
-
-
     while True:
         ret, frame = cam.read()
 
@@ -31,19 +38,45 @@ def main():
         mask = cv2.inRange(hsv, lowBounds, highBounds)
         # mask = cv2.erode(mask, None, iterations=2)
         # mask = cv2.dilate(mask, None, iterations=2)
-        # ballMask = cv2.bitwise_and(blurred, blurred, mask=mask)
 
         imgContours, contours = cvzone.findContours(frame, mask)
 
-        keypoint, des = orb.detectAndCompute(blurred, mask=mask)
-        img_keypoints = cv2.drawKeypoints(blurred, keypoint, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
+        # Process for the first frame
+        if first_frame:
+            fixed_keypoints, des = orb.detectAndCompute(blurred, mask=mask)        
+            if contours:
+                first_area = contours[0]["area"]
+
+            first_frame = False
+        else:
+            keypoint, des = orb.detectAndCompute(blurred, mask=mask) 
+            img_keypoints = cv2.drawKeypoints(blurred, keypoint, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+            # cv2.imshow('keyPoints', img_keypoints)
+
+            img_fixed_keypoints = cv2.drawKeypoints(blurred, fixed_keypoints, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+            # cv2.imshow('fixed keyPoints', img_fixed_keypoints)
+
+            if contours and abs(contours[0]["area"] - first_area) > delta_oscillation_max:
+                
+                if contours[0]["area"] < first_area:
+                    z -= depth_step
+                else:
+                    z += depth_step
+                area = contours[0]["area"]
+                print(f"first area {first_area} area = {area} z = {z}")
+
+
+
+        # Get position and area from contours
+        if contours:
+            data = contours[0]["center"][0], contours[0]["center"][1], contours[0]["area"]
 
 
         cv2.imshow('Img contours', imgContours)
         cv2.imshow('Camera', frame)
         cv2.imshow('Mask', mask)
-        cv2.imshow('keyPoints', img_keypoints)
 
 
         if cv2.waitKey(1) == ord('q'):
